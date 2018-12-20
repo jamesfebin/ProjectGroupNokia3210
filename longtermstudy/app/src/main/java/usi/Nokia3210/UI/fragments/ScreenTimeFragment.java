b6688e2475de4208218deddaeb78188be6af22a5
Michael Denzler <denzlm@usi.ch>
2018-12-20 12:10:09 +0100

    project finlized, all is working

--- longtermstudy/app/src/main/java/usi/Nokia3210/UI/fragments/ScreenTimeFragment.java
+++ longtermstudy/app/src/main/java/usi/Nokia3210/UI/fragments/ScreenTimeFragment.java
@@ -1,4 +1,5 @@
 package usi.Nokia3210.UI.fragments;
+import usi.Nokia3210.MyApplication;
 import usi.Nokia3210.gathering.gatheringServices.ApplicationLogs.CustomUsageStats;
 import usi.Nokia3210.local.database.tables.WiFiTable;
 import usi.Nokia3210.remote.database.upload.AlarmService;
@@ -7,6 +8,7 @@ import android.app.usage.UsageStats;
 import android.app.usage.UsageStatsManager;
 import android.content.ContentValues;
 import android.content.Context;
+import java.util.concurrent.TimeUnit;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -19,7 +21,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import android.app.Application;
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.charts.Chart;
@@ -45,7 +50,9 @@ import android.widget.RelativeLayout;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Comparator;
 import java.util.List;
 
 import usi.Nokia3210.R;
@@ -59,7 +66,8 @@ import usi.Nokia3210.local.database.tables.ApplicationLogsTable;
 import usi.Nokia3210.local.database.tables.UserTable;
 import usi.Nokia3210.remote.database.controllers.SwitchDriveController;
 public class ScreenTimeFragment extends Fragment {
-
+    private Context context;
+    private PackageManager pm;
     private LocalStorageController localController;
     SwitchDriveController switchDriveController;
     String androidID;
@@ -81,55 +89,88 @@ public class ScreenTimeFragment extends Fragment {
 
         pieChart = root.findViewById(R.id.pieChartView);
 
+
         addDataSet();
 
         return root;
     }
 
-    private void addDataSet() {
+    private void addDataSet(){
+
+
+
         //List<PieEntry> entries = new ArrayList<>();
         List<String> labels = new ArrayList<>();
         List<UsageStats> stats =getUsageStatistics(UsageStatsManager.INTERVAL_DAILY);
 
+        // sort stats for TotalTimeInForeground (biggest to smallest)
+//        stats.sort(Comparator.naturalOrder());
 
         ArrayList<PieEntry> entries = new ArrayList<>();
-        entries.add(new PieEntry(4f, 0));
-        entries.add(new PieEntry(8f, 1));
-        entries.add(new PieEntry(6f, 2));
-        entries.add(new PieEntry(12f, 3));
-        entries.add(new PieEntry(18f, 4));
-        entries.add(new PieEntry(9f, 5));
 
+        int count=10;
 
-       // for (UsageStats s : stats)
-       // {
+        for (int i=0;i<stats.size();i++)
+        {
             // stai facendo una sola entri devi fare la entri n volte tante quante le app che usi
             // serve indicie nel for
-       //     entries.add(new PieEntry(s.getTotalTimeInForeground()));
-        //    labels.add(s.getPackageName());
-         //   Log.d("ScreenFrag", s.getPackageName());
-       // }
+            //float hstats =  (stats.get(i).getTotalTimeInForeground()/3600000);
+
+            if (getDurationBreakdown(stats.get(i).getTotalTimeInForeground())>10) {
+
+              entries.add(new PieEntry(getDurationBreakdown(stats.get(i).getTotalTimeInForeground()), getApplicationName(getContext(),stats.get(i).getPackageName())));
+//                entries.add(new PieEntry(stats.get(i).getTotalTimeInForeground(), stats.get(i).getPackageName()));
+                    // to get name only
+//                    String[] appname = stats.get(i).getPackageName().split(".");
+//                    entries.add(new PieEntry(stats.get(i).getTotalTimeInForeground(), appname[appname.length - 1]));
+
+            }
+
+          //  Log.d("ScreenFrag", s.getPackageName());
+        }
+
+
 
 
         // create data set
-        PieDataSet dataset = new PieDataSet(entries, "Applications");
-        dataset.setSliceSpace(2);
-        dataset.setValueTextSize(12);
-
+        PieDataSet dataset = new PieDataSet(entries, "Applications usage");
+        dataset.setSliceSpace(3);
+        dataset.setSelectionShift(5);
+        dataset.setValueTextSize(15);
+       // dataset.setValueFormatter(new PercentFormatter());
         // add colors
         // ArrayList<Integer>colors= new ArrayList<>();
         dataset.setColors(ColorTemplate.COLORFUL_COLORS);
 
+
         // add legend to chart -> does that need to be in xml? is xml complete?
-        //pieChart.getLegend().setEnabled(false);
+        //Legend l = pieChart.getLegend();
+        
+        //l.setPosition(LegendPosition.BELOW_CHART_LEFT);
 
+        //l.setXEntrySpace(7);
+        //l.setYEntrySpace(5);
+        pieChart.getLegend().setEnabled(false);
 
-        //we get error here
+        //animate
+        pieChart.animateY(1500);
+
         // create pie data object
         PieData data = new PieData(dataset);
-        data.setValueFormatter(new PercentFormatter());
-        //set value
-        //set volor
+
+        //enable hole and configure
+        pieChart.setDrawEntryLabels(true);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setHoleColor(android.R.color.transparent);
+        pieChart.setHoleRadius(40);
+        pieChart.setTransparentCircleRadius(10);
+
+        pieChart.setRotationAngle(1);
+        pieChart.setRotationEnabled(true);
+
+
+
+       // pieChart.setUsePercentValues(true);
         pieChart.setData(data);
         pieChart.invalidate();
     }
@@ -153,20 +194,58 @@ public class ScreenTimeFragment extends Fragment {
 
         List<UsageStats> queryUsageStats = mUsageStatsManager.queryUsageStats(intervalType, start, end);
 
-        boolean checking = queryUsageStats.isEmpty();
-
-        Log.d("ScreenFrag2", checking + "");
+//        boolean checking = queryUsageStats.isEmpty();
+//
+//        Log.d("ScreenFrag2", checking + "");
 
         for (int i = 0; i < queryUsageStats.size(); i++) {
             CustomUsageStats customUsageStats = new CustomUsageStats();
             customUsageStats.usageStats = queryUsageStats.get(i);
 
             ApplicationLogData appData = new ApplicationLogData(true);
-            appData.setAppPackage(customUsageStats.usageStats.getPackageName());
             appData.setTotalTimeInForeground(customUsageStats.usageStats.getTotalTimeInForeground());
+            appData.setAppPackage(customUsageStats.usageStats.getPackageName());
         }
 
         return queryUsageStats;
     }
 
+
+
+    private String getApplicationName(Context context, String data) {
+
+        final PackageManager pckManager = context.getPackageManager();
+        ApplicationInfo applicationInformation;
+        try {
+            applicationInformation = pckManager.getApplicationInfo(data, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            applicationInformation = null;
+        }
+
+        String title = (String) (applicationInformation != null ? pckManager.getApplicationLabel(applicationInformation) : "(unknown)");
+        return title;
+
+    }
+
+
+
+
+
+    public static long getDurationBreakdown(long millis) {
+        if(millis < 0) {
+            throw new IllegalArgumentException("Duration must be greater than zero!");
+        }
+
+        long hours = TimeUnit.MILLISECONDS.toHours(millis);
+        millis -= TimeUnit.HOURS.toMillis(hours);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
+        millis -= TimeUnit.MINUTES.toMillis(minutes);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+
+
+        return(minutes);
+    }
+
+
 }
+
